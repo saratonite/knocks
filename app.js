8e79780850a69e5b38860ba1357228d80efa1e22
Sarath <sarath.hacks@gmail.com>
2016-10-06 20:07:34 +0530

    React Implementation

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-var q = document.getElementById('theQ');
-q.textContent  = getRandom();
 
-function getRandom(){
-  var random_pick = Math.floor((Math.random()*quotes.length));
-  return quotes[random_pick];
-}
+
+
+// Container Component
+var App = React.createClass({
+
+  getRandom:function(){
+    var random_pick = Math.floor((Math.random()*this.props.quotes.length));
+    return this.props.quotes[random_pick];
+  },
+
+  render:function(){
+
+    return(
+      <div className="app-container">
+        <div>
+          <h3>Qtz</h3>
+        </div>
+        <Quote quotetext={this.getRandom()}/>
+      </div>
+    );
+
+  }
+});
+
+// theQuote Component
+var Quote = React.createClass({
+  render:function(){
+    return(
+      <div className="qContainer">
+        {this.props.quotetext}
+      </div>
+
+    );
+  }
+});
+
+
+
+
+// Initiate App
+
+ReactDOM.render(<App quotes={quotes}/>,document.getElementById('app'));
