df0c8763cdf3ba8855e04f8fc470771f0052a521
Sarath <sarath.hacks@gmail.com>
2016-10-07 09:23:47 +0530

    Improved UI , Adde knock buton

--- app.js
+++ app.js
@@ -3,20 +3,26 @@
 
 // Container Component
 var App = React.createClass({
-
+  getInitialState:function(){
+    return {randomQuote:{author:null}}
+  },
+  componentDidMount:function(){
+    this.setState({randomQuote:this.getRandom()});
+  },
   getRandom:function(){
     var random_pick = Math.floor((Math.random()*this.props.quotes.length));
     return this.props.quotes[random_pick];
   },
-
+  changeQuote:function(){
+    this.setState({randomQuote:this.getRandom()});
+  },
   render:function(){
 
     return(
       <div className="app-container">
+        <Quote quote={this.state.randomQuote} knock={this.changeQuote}/>
         <div>
-          <h3>Qtz</h3>
         </div>
-        <Quote quotetext={this.getRandom()}/>
       </div>
     );
 
@@ -26,9 +32,16 @@ var App = React.createClass({
 // theQuote Component
 var Quote = React.createClass({
   render:function(){
+
+    var author = this.props.quote.author ? <div className="author"> ~ <i>{this.props.quote.author}</i></div> :null;
     return(
       <div className="qContainer">
-        {this.props.quotetext}
+        {this.props.quote.text}
+        {author}
+
+        <div className="qControls">
+          <button onClick={this.props.knock}>Knock It !!!</button>
+        </div>
       </div>
 
     );
--- quotes.js
+++ quotes.js
@@ -1,11 +1,14 @@
 var quotes = [
-  "Quit worrying about what everyone else is doing. Focus on how you’re helping people.",
-  "Be yourself; everyone else is already taken — Oscar Wilde",
-  "“Forgive. Forget. Life is full of misfortunes.”",
-  "If I’m so smart, why is my brain so dumb?",
-  "Don’t dwell on your weaknesses, everybody else is already doing that for you. — Gary Vaynerchuk",
-  "We do not learn from experience. We learn from reflecting on experience.-John Dewey, American Philosopher",
-  "Kill Them With Kindness.,",
-  "Home Is Where You Are.",
-  "Quality Beats Quantity."
+  {"text":"Quit worrying about what everyone else is doing. Focus on how you’re helping people."},
+  {"text":"Be yourself; everyone else is already taken","author":"Oscar Wilde"},
+  {"text":"Forgive. Forget. Life is full of misfortunes."},
+  {"text":"If I’m so smart, why is my brain so dumb?"},
+  {"text":"Don’t dwell on your weaknesses, everybody else is already doing that for you","author":"Gary Vaynerchuk"},
+  {"text":"We do not learn from experience. We learn from reflecting on experience","author":"John Dewey","author_description":"American Philosopher"},
+  {"text":"Kill Them With Kindness"},
+  {"text":"Home Is Where You Are"},
+  {"text":"Quality Beats Quantity"},
+  {"text":"Life’s a game, play it","author":"Mother Teresa"},
+  {"text":"First they ignore you. Then they laugh at you. Then they fight you. Then you win"},
+  {"text":"The more you learn, the more you can learn."}
 ]
