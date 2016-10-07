7da3e04314a9327ffdef07003508bd4b1c5a2278
Sarath <sarath.hacks@gmail.com>
2016-10-07 13:21:23 +0530

    Added colored quotes text , moved quotes.js to data directory

--- app.js
+++ app.js
@@ -4,14 +4,19 @@
 // Container Component
 var App = React.createClass({
   getInitialState:function(){
-    return {randomQuote:{author:null}}
+    return {randomQuote:{author:null},textColor:{code:'red'}};
   },
   componentDidMount:function(){
     this.setState({randomQuote:this.getRandom()});
+    console.log(this.props.colors);
   },
   getRandom:function(){
     var random_pick = Math.floor((Math.random()*this.props.quotes.length));
     return this.props.quotes[random_pick];
+  },
+  getRandomColor:function(){
+    var random_pick = Math.floor((Math.random()*this.props.colors.length));
+    return this.props.colors[random_pick];
   },
   changeQuote:function(){
     this.setState({randomQuote:this.getRandom()});
@@ -20,7 +25,7 @@ var App = React.createClass({
 
     return(
       <div className="app-container">
-        <Quote quote={this.state.randomQuote} knock={this.changeQuote}/>
+        <Quote quote={this.state.randomQuote} knock={this.changeQuote} color={this.getRandomColor()}/>
         <div>
         </div>
       </div>
@@ -34,8 +39,9 @@ var Quote = React.createClass({
   render:function(){
 
     var author = this.props.quote.author ? <div className="author"> ~ <i>{this.props.quote.author}</i></div> :null;
+    var qStyles = {color:this.props.color.code};
     return(
-      <div className="qContainer">
+      <div className="qContainer" style={qStyles}>
         {this.props.quote.text}
         {author}
 
@@ -53,4 +59,4 @@ var Quote = React.createClass({
 
 // Initiate App
 
-ReactDOM.render(<App quotes={quotes}/>,document.getElementById('app'));
+ReactDOM.render(<App quotes={quotes} colors={__colors}/>,document.getElementById('app'));
---
+++ data/colors.js
@@ -0,0 +1,13 @@
+var __colors = [
+  {"name":"f56","code":"#f56"},
+  {"name":"yellow","code":"yellow"},
+  {"name":"Humming Bird","code":"#C5EFF7"},
+  {"name":"h","code":"#5d9"},
+  {"name":"Ecstasy","code":"#F9690E"},
+  {"code":"#BF55EC"},
+  {"name":"Clouds","code":"#ecf0f1"},
+  {"name":"Alizarin","code":"#e74c3c"},
+  {"name":"SunFlower","code":"#f1c40f"},
+  {"name":"Carrot","code":"#e67e22"},
+  {"name":"Emaralod","code":"#2ecc71"}
+]
